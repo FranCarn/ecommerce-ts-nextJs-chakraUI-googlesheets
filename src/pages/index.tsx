1c3f17beedc4d9af005a48857721aa04113fc1f7
@FranCarn <carnevalefranco99@gmail.com>
2023-03-04 19:05:25 -0300

    feat(index.tsx): feature to connect with wpp api ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,77 @@
 import api from "@/product/api";
 import { Product } from "@/product/types";
-import { Grid, Stack, Text } from "@chakra-ui/react";
+import { Button, Grid, Image, Link, Stack, Text } from "@chakra-ui/react";
 import { GetStaticProps } from "next";
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 interface Props {
   products: Product[];
+  textToSend: () => String;
+}
+
+function parseCurrency(value: number): string {
+  return value.toLocaleString("es-AR", {
+    style: "currency",
+    currency: "ARS",
+  });
 }
 
 const IndexRoute: React.FC<Props> = ({ products }) => {
+  const [cart, setCart] = useState<Product[]>([]);
+
+  function handleAddtoCart(product: Product) {
+    setCart((cart) => cart.concat(product));
+  }
+  const textToSend = React.useMemo(() => {
+    return cart
+      .reduce(
+        (message, product) =>
+          message.concat(
+            `*${product.title} - ${parseCurrency(product.price)}\n`
+          ),
+        ``
+      )
+      .concat(
+        `\n Total: ${parseCurrency(
+          cart.reduce((total, product) => total + product.price, 0)
+        )}`
+      );
+  }, [cart]);
   return (
-    <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
-      {products.map((product) => (
-        <Stack backgroundColor="gray.100" key={product.id}>
-          <Text>{product.title}</Text>
-        </Stack>
-      ))}
-    </Grid>
+    <Stack>
+      <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
+        {products.map((product) => (
+          <Stack backgroundColor="gray.100" key={product.id}>
+            <Text>{product.category}</Text>
+            <Text>{product.title}</Text>
+            <Image
+              src={product.image}
+              alt={product.title}
+              width={300}
+              height={150}
+              objectFit="scale-down"
+            />
+            <Text>{product.description}</Text>
+            <Text>{parseCurrency(product.price)}</Text>
+            <Button onClick={() => handleAddtoCart(product)} colorScheme="blue">
+              Agregar al carrito
+            </Button>
+          </Stack>
+        ))}
+      </Grid>
+      {cart.length && (
+        <Link
+          href={`https://wa.me/54911414141?text=${encodeURIComponent(
+            textToSend
+          )}`}
+          isExternal
+        >
+          <Button colorScheme="whatsapp">
+            Completar pedido ({cart.length}) productos
+          </Button>
+        </Link>
+      )}
+    </Stack>
   );
 };
 
